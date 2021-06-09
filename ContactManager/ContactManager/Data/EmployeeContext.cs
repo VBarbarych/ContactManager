13ea5ed5d99ddadfe3dc42923ede2479d0c1ac0a
VBarbarych <37077122+VBarbarych@users.noreply.github.com>
2021-06-09 11:18:07 +0300

    Add DbContext file, add string connection to db

---
+++ ContactManager/ContactManager/Data/EmployeeContext.cs
@@ -0,0 +1,24 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
