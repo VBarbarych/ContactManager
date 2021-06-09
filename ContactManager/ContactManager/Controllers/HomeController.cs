7d990a82f5afe440e3afd3cbbc4058b5c1f82a9b
VBarbarych <37077122+VBarbarych@users.noreply.github.com>
2021-06-09 19:52:30 +0300

    Add edit/delete functions, add view for these methods

--- ContactManager/ContactManager/Controllers/HomeController.cs
+++ ContactManager/ContactManager/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using ContactManager.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -40,6 +41,86 @@ namespace ContactManager.Controllers
             return View(emp);
         }
 
+
+        // GET
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await employeeService.GetItemByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            
+            return View(employee);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DateOfBirth,IsMarried,Phone,Salary")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                   
+                    await employeeService.EditItemAsync(employee);
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (employeeService.GetItemByIdAsync(id) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            
+            return View(employee);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await employeeService.GetItemByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await employeeService.GetItemByIdAsync(id);
+            await employeeService.DeleteItemAsync(employee);
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult Privacy()
         {
